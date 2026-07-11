5a407bf484d824cfdd85544c2fdfd514ee023b3a
Sahasra Preetam <sahasrapreetham23@gmail.com>
2026-07-11 14:45:30 +0530

    Add Java basics programs: Main, Variables, Star Pattern, and Scanner Input

---
+++ 01-Basics/variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("The sum of a and b is: " + sum);
+        int diff = b-a;
+        System.out.println("The difference of b and a is: " + diff);
+        int mul =a*b;
+        System.out.println("The multiplication of a and b is: " + mul);
+    }
+}
