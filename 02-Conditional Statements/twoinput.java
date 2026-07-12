3540f167dc125db5de678cc92d82d3747020b760
Sahasra Preetam <sahasrapreetham23@gmail.com>
2026-07-12 13:55:50 +0530

    Taking two inputs and following conditions

---
+++ 02-Conditional Statements/twoinput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class twoinput {
+    public static void main (String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if (x==y) {
+            System.out.println("Equal");
+        } else if(x > y) {
+                System.out.println("a is greater than b");
+        }
+             else{
+                System.out.println("a is lesser than b");
+           
+             }
+
+
+
