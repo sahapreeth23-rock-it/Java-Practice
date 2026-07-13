fc754a4981a9ccaac51a6c165d4bcd8dc5803fec
Sahasra Preetam <sahasrapreetham23@gmail.com>
2026-07-13 21:07:29 +0530

    Implementing counter variable in loops

---
+++ 03-Loops/counter.java
@@ -0,0 +1,11 @@
+public class counter {
+    public static void main(String[] args) {
+        //counter ++ => counter = counter + 1
+        //for(int counter = 0;counter < 11; counter ++) {
+        //  System.out.print(counter +" ");
+        //}
+        for ( int i = 0 ; i < 11 ; i++ ) {
+            System.out.println(i);
+        } 
+    }
+}
